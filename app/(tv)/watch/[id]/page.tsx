969ffe0bf7dd99b401f7b82f5769f9315cdb5ab6
lazar <lazar.trifunovic@boopro.tech>
2024-08-30 21:01:31 +0200

    .

--- app/(tv)/watch/[id]/page.tsx
+++ app/(tv)/watch/[id]/page.tsx
@@ -27,7 +27,7 @@ export default function WatchTournament({
         <h1>{tournament?.title}</h1>
       </div>
       <article className="tv-tournament">
-        {t ? (
+        {!t ? (
           <Suspense fallback={null}>
             {Object.values(groups)?.map((n: any, i: number) => (
               <Group key={i} {...n} />
